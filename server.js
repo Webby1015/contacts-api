03035cd0b786e0824f79dc4426ca25f0d5640d3f
Webby1015 <vedantabhishek@gmail.com>
2024-03-06 04:26:38 +0530

    .....

--- server.js
+++ server.js
@@ -3,52 +3,9 @@ const app = express();
 const dotenv = require('dotenv').config()
 const port = process.env.port;
 
-app.get('/', (req, res) => {
-    res.send("hello");
 
-});
-
-app.use('/api/contacts',require("./routes/ContactsRoutes.js"))
-
-
-
-// app.get('/', (req, res) => {
-//     res.json({
-//         "routes": [
-//             {
-//                 "method": "GET",
-//                 "path": "/",
-//                 "description": "Get all contacts",
-//                 "handler": "getAllContacts"
-//             },
-//             {
-//                 "method": "GET",
-//                 "path": "/:id",
-//                 "description": "Get one contact",
-//                 "handler": "getOneContact"
-//             },
-//             {
-//                 "method": "POST",
-//                 "path": "/",
-//                 "description": "Create contact",
-//                 "handler": "createContact"
-//             },
-//             {
-//                 "method": "PUT",
-//                 "path": "/:id",
-//                 "description": "Update contact",
-//                 "handler": "updateContact"
-//             },
-//             {
-//                 "method": "DELETE",
-//                 "path": "/:id",
-//                 "description": "Delete contact",
-//                 "handler": "deleteContact"
-//             }
-//         ]
-//     });
-
-//   });
+// app.use('/api/contacts',require("./routes/ContactsRoutes.js"))
+app.use('',require("./routes/ContactsRoutes.js"))
 
 app.listen(port,()=>{
     console.log(`Listening at ${port}`)
